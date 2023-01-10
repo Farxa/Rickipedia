95b15b16d13a0e1b6dd2bcf5153eaacb4d9e9ef3
Rahaf-GCT <r.abualhassan@globalct.com>
2023-01-10 21:17:22 +0100

    better pagNum component

--- Rickipedia/src/components/PageNums.tsx
+++ Rickipedia/src/components/PageNums.tsx
@@ -3,105 +3,59 @@ import { Link, Button } from '@mui/material';
 import styles from './pagination.module.scss';
 import classNames from 'classnames';
 
-// type ChildProps = {
-// 	count:number
-// }
-
 export interface Props {
 	page: number;
 	totalPages: number;
 	handlePagination: (page: number) => void;
 }
 
-// const PageNums: React.FunctionComponent<ChildProps> = ({ count }) =>{
+
+
 const PageNums: React.FC<Props> = ({ page, totalPages, handlePagination }) => {
-	// const pageNumbers = [];
+    const MAX_PAGE_ITEM = 5;
+    const startPage = (page - Math.floor(MAX_PAGE_ITEM / 2)) < 1 ? 1 : (page - Math.floor(MAX_PAGE_ITEM / 2));
+    const endPage =  (page + Math.floor(MAX_PAGE_ITEM / 2)) > totalPages ? totalPages : (page + Math.floor(MAX_PAGE_ITEM / 2));
+    const hasHiddenBefore = startPage !== 1;
+    const hasHiddenAfter = endPage !== totalPages;
 
-	// for (let i = 1; i <= count; i++) {
-	//   pageNumbers.push(i);
-	// }
-
-	return (
-		<div className={styles.pagination}>
-			<div className={styles.paginationWrapper}>
-				{page !== 1 && (
-					<Button>
-						<Link
-							onClick={() => handlePagination(page - 1)}
-							className={classNames([styles.pageItem, styles.sides].join(' '))}>
-							&lt;
-						</Link>
-					</Button>
-				)}
-				<Button>
-					<Link
-						onClick={() => handlePagination(1)}
-						className={classNames(styles.pageItem, {
-							[styles.active]: page === 1,
-						})}>
-						{1}
-					</Link>
-				</Button>
-
-				{page > 3 && <div className={styles.separator}>...</div>}
-				{page === totalPages && totalPages > 3 && (
-					<Button>
-						<Link className={styles.pageItem} onClick={() => handlePagination(page - 2)}>
-							{page - 2}
-						</Link>
-					</Button>
-				)}
-				{page > 2 && (
-					<Button>
-						<Link className={styles.pageItem} onClick={() => handlePagination(page - 1)}>
-							{page - 1}
-						</Link>
-					</Button>
-				)}
-				{page !== 1 && page !== totalPages && (
-					<Button>
-						<Link
-							className={[styles.pageItem, styles.active].join(' ')}
-							onClick={() => handlePagination(page)}>
-							{page}
-						</Link>
-					</Button>
-				)}
-				{page < totalPages - 1 && (
-					<Button>
-						<Link className={styles.pageItem} onClick={() => handlePagination(page + 1)}>
-							{page + 1}
-						</Link>
-					</Button>
-				)}
-				{page === 1 && totalPages > 3 && (
-					<Button>
-						<Link className={styles.pageItem} onClick={() => handlePagination(page + 2)}>
-							{page + 2}
-						</Link>
-					</Button>
-				)}
-				{page < totalPages - 2 && <div className={styles.separator}>...</div>}
-				<Button>
-					<Link
-						className={classNames(styles.pageItem, {
-							[styles.active]: page === totalPages,
-						})}
-						onClick={() => handlePagination(totalPages)}>
-						{totalPages}
-					</Link>
-				</Button>
-				{page !== totalPages && (
-					<Button>
-						<Link
-							className={[styles.pageItem, styles.sides].join(' ')}
-							onClick={() => handlePagination(page + 1)}>
-							&gt;
-						</Link>
-					</Button>
-				)}
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.pagination}>
+            <div className={styles.paginationWrapper}>
+                {page !== 1 && (
+                <Button>
+                    <Link
+                    onClick={() => handlePagination(page - 1)}
+                    className={classNames([styles.pageItem, styles.sides].join(' '))}>
+                        &lt;
+                    </Link>
+                </Button>
+                )}
+                {hasHiddenBefore && <div className={styles.separator}>...</div>}
+                {Array.from({ length: (endPage - startPage) + 1}, (_, i) => startPage + i).map(num => (
+                <Button>
+                    <Link
+                    onClick={() => handlePagination(num)}
+                    className={classNames([styles.pageItem, {
+                        [styles.active]: num === page
+                    }])}
+                    >
+                        {num}
+                    </Link>
+                </Button>
+                ))}
+                {hasHiddenAfter && <div className={styles.separator}>...</div>}
+                {page !== totalPages && (
+                <Button>
+                    <Link
+                    onClick={() => handlePagination(page + 1)}
+                    className={classNames([styles.pageItem, styles.sides].join(' '))}>
+                        &gt;
+                    </Link>
+                </Button>
+                )}
+            </div>
+        </div>
+    );
 };
+
 export default PageNums;
